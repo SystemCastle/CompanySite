5e804c528e44273b8b6d4f45061ec3ed8a434040
Muhammad Khalil <muhammad.khalil@systemcastle.com>
2026-07-17 14:39:04 +0500

    17/07/2026

--- app/1.0/components/HeroV2.tsx
+++ app/1.0/components/HeroV2.tsx
@@ -156,7 +156,6 @@ export default function HeroV2() {
     return () => {
       first.removeEventListener('loadeddata', handleLoadedData)
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
